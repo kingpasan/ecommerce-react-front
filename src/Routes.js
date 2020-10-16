8a4bc46bbaaef1bae69189d86e5ada86a8160d62
Pasan Semage <pmsemage@gmail.com>
2020-10-16 13:34:28 +0530

    navigation added

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Menu from './core/Menu';
 import Home from './core/Home'
 import Signup from './user/Signup';
 import Signin from './user/Signin';
@@ -7,6 +8,7 @@ import Signin from './user/Signin';
 const Routes = () => {
     return (
         <BrowserRouter>
+            <Menu />
             <Switch>
                 <Route path="/" exact component={Home} />
                 <Route path="/signin" exact component={Signin} />
